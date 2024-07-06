2de128b6ac59bf62004dd08211013e159541e415
MK7346 <163279454+MK7346@users.noreply.github.com>
2024-07-06 14:30:54 +0200

    added: root_dir and file creation

--- cs225/225.c
+++ cs225/225.c
@@ -59,7 +59,24 @@ struct directory_entry {
 //we declare 2 Global Variables for Superblock and Inode Table
 struct superblock sb;
 struct inode *inode_table;
+//the following is for the read adn write operations
+unsigned char disk[TOTAL_BLOCKS][BLOCK_SIZE];
 
+void write_block(int block_index, void *data){
+    if(block_index<0 || block_index >= TOTAL_BLOCKS){
+        printf("block index is invalid \n");
+        return;
+    }
+    memcpy(disk[block_index],data, BLOCK_SIZE);
+}
+void read_block(int block_index, void *data){
+    if(block_index<0 || block_index>= TOTAL_BLOCKS){
+        printf("block index is invalid \n");
+        return;
+    }
+    memcpy(data, disk[block_index], BLOCK_SIZE);
+}
+//initialize the file system
 void filesystem_initialization(){
     //the following initializes the superblock
     sb.s_inodes_count = TOTAL_INODES;
@@ -78,11 +95,133 @@ void filesystem_initialization(){
     inode_table = (struct inode *)malloc(INODE_TABLE_SIZE * sizeof(struct inode));
     memset(inode_table, 0, INODE_TABLE_SIZE * sizeof(struct inode));
 }
+//allocating an inode and returning its index
+int allocate_inode(){
+    for(int i=0; i< INODE_TABLE_SIZE;i++){
+        if (inode_table[i].i_mode==0){
+            inode_table[i].i_mode =1;
+            sb.s_free_inodes_count--;
+            return i;
+        }
+    }
+    return -1;
+}
+//deallocating an inode
+void deallocate_inode(int inode_index){
+    inode_table[inode_index].i_mode =0;
+    sb.s_free_inodes_count++;
+}
+//allpocating a block and returning its index
+int allocate_block(){
+    static int next_free_block=0;
+    if (next_free_block<sb.s_blocks_count){
+        sb.s_free_blocks_count--;
+        return next_free_block++;
+    }
+    return -1;
+}
+//deallocate a block
+void deallocate_block(int block_index){
+    sb.s_free_blocks_count++;
+}
+//CREATING A ROOT DIRECTORY
+void create_root_directory(){
+    int root_inode_index = allocate_inode();
+    if (root_inode_index == -1){
+        printf("allocation of inode for rrot directory failed.\n");
+        exit(1);
+    }
+    struct inode *root_inode = &inode_table[root_inode_index];
+    root_inode->i_mode = 0x4000; // this sets the mode bits of an inode to mark it as a directory
+    root_inode->i_uid =0;
+    root_inode->i_gid =0;
+    root_inode->i_size = BLOCK_SIZE;
+    root_inode->i_atime = root_inode->i_ctime = root_inode->i_mtime = time(NULL);
+    root_inode->i_links_count =2;
+    root_inode->i_blocks =1;
+    
+    int block_index = allocate_block();
+    if (block_index == -1){
+        printf("allocation of block for root directory failed.\n");
+        exit(1);
+    }
+    root_inode->i_block[0]=block_index;
+
+    struct directory_entry *root_dir =(struct directory_entry *)malloc(BLOCK_SIZE);
+    memset(root_dir, 0, BLOCK_SIZE);
+    root_dir[0].inode = root_inode_index;
+    root_dir[0].rec_len = sizeof(struct directory_entry);
+    root_dir[0].name_len =1;
+    root_dir[0].file_type =2;
+    strcpy(root_dir[0].name, ".");
+    root_dir[1].inode = root_inode_index;
+    root_dir[1].rec_len = sizeof(struct directory_entry);
+    root_dir[1].name_len = 2;
+    root_dir[1].file_type =2;
+    strcpy(root_dir[1].name, "..");
+
+    //write the root directry to block
+    write_block(block_index, root_dir);
+    free(root_dir);
+
+}
+//file creation
+int create_file(const char *filename, int parent_inode_index) {
+    int inode_index = allocate_inode();
+    if (inode_index == -1) {
+        printf("allocation of inode for file failed.\n");
+        return -1;
+    }
+
+    struct inode *file_inode = &inode_table[inode_index];
+    file_inode->i_mode = 0x8000; // Regular file
+    file_inode->i_uid = 0;
+    file_inode->i_gid = 0;
+    file_inode->i_size = 0;
+    file_inode->i_atime = file_inode->i_ctime = file_inode->i_mtime = time(NULL);
+    file_inode->i_links_count = 1;
+    file_inode->i_blocks = 0;
+
+    struct inode *parent_inode = &inode_table[parent_inode_index];
+    struct directory_entry *parent_dir = (struct directory_entry *)malloc(BLOCK_SIZE);
+    memset(parent_dir, 0, BLOCK_SIZE);
+
+    // Read parent directory block
+    read_block(parent_inode->i_block[0], parent_dir);
+
+    int entry_offset = 0;
+    while (entry_offset < BLOCK_SIZE) {
+        struct directory_entry *entry = (struct directory_entry *)((char *)parent_dir + entry_offset);
+        if (entry->inode == 0) { // Found a free entry
+            entry->inode = inode_index;
+            entry->rec_len = sizeof(struct directory_entry);
+            entry->name_len = strlen(filename);
+            entry->file_type = 1; // Regular file
+            strcpy(entry->name, filename);
+            break;
+        }
+        entry_offset += entry->rec_len;
+    }
+
+    // Write parent directory block back
+    write_block(parent_inode->i_block[0], parent_dir);
+
+    free(parent_dir);
+    return inode_index;
+}
+
 
 int main()
 {
     filesystem_initialization();
     printf("file stuff successful. \n");
+    // Create a new file in the root directory
+    int file_inode_index = create_file("example.txt", 0);
+    if (file_inode_index != -1) {
+        printf("File 'example.txt' created successfully with inode index %d.\n", file_inode_index);
+    } else {
+        printf("Failed to create file 'example.txt'.\n");
+    }
     return 0;
 }
 
